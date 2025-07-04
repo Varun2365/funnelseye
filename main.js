31f16e41c0ec7283b144124c318f71ababd9223e
Varun2365 <varun.kumar.sharma.2365@gmail.com>
2025-07-04 13:29:25 +0530

    PORT 8000

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
 
 
 // 🌍 Define Server Port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 
 // --- Helper function to print API routes in a formatted table ---
